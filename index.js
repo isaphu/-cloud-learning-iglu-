c7a54579665f1cffb3a83c6d7fa69f55592438b8
isaphu <uronohan@gmail.com>
2020-11-23 07:26:03 +0000

    adding files

---
+++ index.js
@@ -0,0 +1,20 @@
+//this is my ten-thousands line JS code
+console.log("My name is Isa");
+
+//to declare a variable
+let name = "Isa";
+console.log(name);
+
+//naming things:
+//can't be a reserved keyword
+//meaningful label
+//can't start with number
+//can't contain space or hyphen
+// name or case-sensitive use camelCase
+
+let firstName = "Isa";
+let lastName = "Phuyuthanon";
+console.log(firstName + " " + lastName);
+
+const interestRate = 0.3;
+console.log(interestRate);
