1557d880d8d1a1a5dfedba5cc4217ba761ae0cdd
isaphu <uronohan@gmail.com>
2020-11-24 09:03:54 +0000

    switch...and case

--- controlflow.js
+++ controlflow.js
@@ -13,3 +13,31 @@ else if (wage >=2000 && wage <5000)
     console.log("employee has excellent benefit and pay, consider case by case"); 
 else
     console.log("consider increasing upon feedback from manager"); 
+
+
+//switch..and case
+
+let employeePosition = 'manager';
+
+switch (employeePosition) {
+    case 'manager':
+    console.log('Manager Level 02');
+    break;
+    case 'officer':
+    console.log('Officer Level 01');
+    break;
+    default: 
+    console.log('Unknown User');
+}
+
+let employeePosition2 = "officer";
+let wage2 = 11000;
+if (employeePosition == 'officer' && wage2 >= 100 || wage2 <2500)
+    console.log('This role need to be consider for wage increase');
+else if (employeePosition == 'officer' && wage2 >= 2500 || wage2 < 5000)
+console.log('This role doesn\'t nedd to be consider for wage increase');
+else if (employeePosition == 'manager' && wage2 <10000)
+console.log('This role need to be promoted but doesn\'t need to consider for wage increase');
+else 
+console.log('consider case by case')
+
