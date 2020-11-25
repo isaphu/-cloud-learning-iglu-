c70e336c96ff0b683b9745ef3f8785304a6f07f5
isaphu <uronohan@gmail.com>
2020-11-25 08:56:49 +0000

    break and continue

--- controlflow.js
+++ controlflow.js
@@ -1,93 +1,108 @@
 //if..else
 //if wage is 100 - 1000: underpaid
 //if wage is 1000-2000: ok
-//if wage is 2000-5000: excellent paid 
-// otherwise: no need to consider 
+//if wage is 2000-5000: excellent paid
+// otherwise: no need to consider
 
 let wage = 2;
-if (wage >= 0 && wage < 1000 ) 
-    console.log("must consider for increasing pay and benefits");
-else if (wage >=1000 && wage < 2000) 
-    console.log("must consider for increasign pay");
-else if (wage >=2000 && wage <5000) 
-    console.log("employee has excellent benefit and pay, consider case by case"); 
-else
-    console.log("consider increasing upon feedback from manager"); 
-
+if (wage >= 0 && wage < 1000)
+  console.log("must consider for increasing pay and benefits");
+else if (wage >= 1000 && wage < 2000)
+  console.log("must consider for increasign pay");
+else if (wage >= 2000 && wage < 5000)
+  console.log("employee has excellent benefit and pay, consider case by case");
+else console.log("consider increasing upon feedback from manager");
 
 //switch..and case
 
-let employeePosition = 'manager';
+let employeePosition = "manager";
 
 switch (employeePosition) {
-    case 'manager':
-        console.log('Manager Level 02');
+  case "manager":
+    console.log("Manager Level 02");
     break;
-    case 'officer':
-        console.log('Officer Level 01');
+  case "officer":
+    console.log("Officer Level 01");
     break;
-    default: 
-        console.log('Unknown User');
+  default:
+    console.log("Unknown User");
 }
 
 let employeePosition2 = "officer";
 let wage2 = 11000;
-if (employeePosition == 'officer' && wage2 >= 100 || wage2 <2500)
-    console.log('This role need to be consider for wage increase');
-else if (employeePosition == 'officer' && wage2 >= 2500 || wage2 < 5000)
-    console.log('This role doesn\'t nedd to be consider for wage increase');
-else if (employeePosition == 'manager' && wage2 <10000)
-    console.log('This role need to be promoted but doesn\'t need to consider for wage increase');
-else 
-    console.log('consider case by case')
+if ((employeePosition == "officer" && wage2 >= 100) || wage2 < 2500)
+  console.log("This role need to be consider for wage increase");
+else if ((employeePosition == "officer" && wage2 >= 2500) || wage2 < 5000)
+  console.log("This role doesn't nedd to be consider for wage increase");
+else if (employeePosition == "manager" && wage2 < 10000)
+  console.log(
+    "This role need to be promoted but doesn't need to consider for wage increase"
+  );
+else console.log("consider case by case");
 
-// for loops 
+// for loops
 //for (initialExpression; condition and compare the value with initial; increamentExpression)
 
-let favsColor = ['yello', 'red', 'brown', 'blue','green','grey','purple','pink'];
+let favsColor = [
+  "yello",
+  "red",
+  "brown",
+  "blue",
+  "green",
+  "grey",
+  "purple",
+  "pink",
+];
 for (let i = 0; i < 2; i++) {
-    console.log(favsColor[2], i);
+  console.log(favsColor[2], i);
 }
 
 for (let i = 5; i >= 1; i--) {
-    if(i % 2 !== 0)
-    console.log(i);
+  if (i % 2 !== 0) console.log(i);
 }
 
-//while loops 
+//while loops
 let i = 0;
 /* while(condition) {
     statement 
     incrementExpression
 } */
 
-while(i <= 5) {
-    if (i % 2 !== 0) console.log(i);
-    i++;
+while (i <= 5) {
+  if (i % 2 !== 0) console.log(i);
+  i++;
 }
 
 // do...while loop (always execute the code one even if the condition is false)
-let s = 2
+let s = 2;
 do {
-    if (s % 2 !== 0) console.log(s);
-    s++;
+  if (s % 2 !== 0) console.log(s);
+  s++;
 } while (s <= 5);
 
-
-//for in loop it iterate over the porperties in an object 
+//for in loop it iterate over the porperties in an object
 const personInfo = {
-    name: 'Mary',
-    age: 26
+  name: "Mary",
+  age: 26,
 };
 
-for(let key in personInfo)
-    console.log(key, personInfo[key])
+for (let key in personInfo) console.log(key, personInfo[key]);
 
-const favsFurniture = ['sofa','chair','stool'];
-for (let index in favsFurniture)
-    console.log(index, favsFurniture[index])
+const favsFurniture = ["sofa", "chair", "stool"];
+for (let index in favsFurniture) console.log(index, favsFurniture[index]);
 
 //for-of loop it iterate over an element or an item in an array
-const colors = ['red','green', 'blue']
-for (let color of colors)
-    console.log(color);
+const colors = ["red", "green", "blue"];
+for (let color of colors) console.log(color);
+
+//break and continue
+let q = 0;
+while (q <= 5) {
+  // if (q === 5) break;
+  if (q % 2 === 0) {
+    q++;
+    continue;
+  }
+  console.log(q);
+  q++;
+}
