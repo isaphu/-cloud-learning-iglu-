8b8647c8cd24a93de3f6c04a46bb5a155abdf61a
isaphu <uronohan@gmail.com>
2020-12-09 08:34:01 +0000

    how to find an element that exist in the array

--- array.js
+++ array.js
@@ -9,3 +9,9 @@ numbers.unshift(1, 2);
 //middle 
 numbers.splice(3, 0, 'hello', 'world')
 console.log(numbers)
+
+//finding element exist in an array 
+const number2 = [1, 2, 3, 1, 4];
+
+console.log(number2.indexOf(1, 2));
+console.log(number2.lastIndexOf(1));
