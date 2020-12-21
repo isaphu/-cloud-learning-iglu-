bce7d7c32cdc2c29dc69ac7bfaa3de2e4951106b
isaphu <uronohan@gmail.com>
2020-12-21 02:43:41 +0000

    adding error handling function try and catch

--- function.js
+++ function.js
@@ -50,17 +50,42 @@ console.log(sum(1, 2, 3, 4, 6, 7, 8));
 //getter => access properties
 //setter => change (mutate) them 
 
+// const person = {
+//     firstName: 'Mosh',
+//     lastName: 'Hamedani',
+//     get fullname() {
+//         return `${person.firstName} ${person.lastName}`
+//     },
+//     set fullName(value) {
+//         const parts = value.split(' ');
+//         this.firstName = parts[0];
+//         this.lastName = parts[1];
+//     }
+// };
+
+// console.log(person)
+
+//try and catch, basic error handling 
 const person = {
     firstName: 'Mosh',
     lastName: 'Hamedani',
-    get fullname() {
-        return `${person.firstName} ${person.lastName}`
-    },
-    set fullName(value) {
+    set fullname(value) {
+        if (typeof value !== 'string')
+            throw new Error('value is not a string');
+
         const parts = value.split(' ');
+        if (parts.length !== 2)
+            throw new Error('Enter a first and last name')
         this.firstName = parts[0];
         this.lastName = parts[1];
     }
 };
 
+try {
+    person.fullName = null;
+} catch (e) {
+    alert(e);
+}
+
 console.log(person)
+
